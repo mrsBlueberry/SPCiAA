88354e8e0899855ba0be9333f6c077a126101ff0
Anna <anna.jagodzinska91@gmail.com>
2014-10-15 00:21:42 +0200

    power

--- ex1/src/Executor.java
+++ ex1/src/Executor.java
@@ -4,48 +4,49 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
 /**
  * @(#)Executor.java
- *
- *
+ * 
+ * 
  * @author
  * @version 1.00 2011/6/13
  */
 
-
 class Executor extends Thread {
+
+	private int power = 3;
+
 	public synchronized void run() {
 		Counter counter = new Counter(this);
-		Vertex S = new Vertex(null,null,null,"S");
-		//[(P1)]
-		P1 p1 = new P1(S,counter);
+		Vertex S = new Vertex(null, null, null, "S");
+		// [(P1)]
+		P1 p1 = new P1(S, counter);
 		p1.start();
 		counter.release();
-		
+
 		make2kTree(p1, counter, 1);
 	}
 
 	private void make2kTree(Production production, Counter counter, int k) {
 		Queue<Production> queue = new LinkedList();
 		queue.add(production);
-		while(!queue.isEmpty()){
+		while (!queue.isEmpty()) {
 			Iterator<Production> i = queue.iterator();
 			List<Production> l = new LinkedList();
-			while(i.hasNext()){
+			while (i.hasNext()) {
 				l.add(queue.poll());
-				
+
 			}
-			for(Production p : l){
-				if(k<3){
+			for (Production p : l) {
+				if (k < power) {
 					P2 p2a = new P2(p.m_vertex.m_left, counter);
 					P2 p2b = new P2(p.m_vertex.m_right, counter);
-					
+
 					p2a.start();
 					p2b.start();
 					queue.add(p2b);
 					queue.add(p2a);
-				}else{
+				} else {
 					P3 p3a = new P3(production.m_vertex.m_left, counter);
 					P3 p3b = new P3(production.m_vertex.m_right, counter);
 					p3a.start();
@@ -56,9 +57,8 @@ class Executor extends Thread {
 			counter.release();
 
 			++k;
-			
+
 		}
 
 	}
 }
-
--- ex1/src/Production.java
+++ ex1/src/Production.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @(#)Production.java
  *

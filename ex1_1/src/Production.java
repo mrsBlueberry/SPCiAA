6cf5445cf361e222750b099e19038703c7e66277
Anna <anna.jagodzinska91@gmail.com>
2014-10-23 14:18:52 +0200

    finally works!!!

--- ex1_1/src/Production.java
+++ ex1_1/src/Production.java
@@ -198,6 +198,7 @@ class Eroot extends Production {
 
 	Vertex apply(Vertex T) {
 		System.out.println("Eroot");
+		
 		T.m_b[1] /= T.m_a[1][1];
 		T.m_a[1][2] /= T.m_a[1][1];
 		T.m_a[1][1] /= T.m_a[1][1];
@@ -217,10 +218,10 @@ class Eroot extends Production {
 		T.m_b[0] /= T.m_a[0][0];
 		T.m_a[0][0] /= T.m_a[0][0];
 
-		T.m_x[2] = T.m_b[2] / T.m_a[2][2];
-		T.m_x[1] = (T.m_b[1] - T.m_a[1][2] * T.m_x[2]) / T.m_a[1][1];
-		T.m_x[0] = (T.m_b[0] - T.m_a[0][1] * T.m_x[1] - T.m_a[0][2] * T.m_x[2])
-				/ T.m_a[0][0];
+		T.m_x[2] = T.m_b[2];
+		T.m_x[1] = T.m_b[1];
+		T.m_x[0] = T.m_b[0];
+		
 		return T;
 	}
 }
@@ -234,38 +235,18 @@ class BS extends Production {
 		System.out.println("BS");
 		if (T.m_label.equals("node"))
 			return T;
-//		T.m_b[0]=(T.m_b[0]-T.m_a[0][1]*T.m_b[1]-T.m_a[0][2]*T.m_b[2])/T.m_a[0][0];
-//		T.m_a[0][0] = 1.0;
-//		T.m_a[0][1] = 0.0;
-//		T.m_a[0][2] = 0.0;
-//		T.m_left.m_a[1][0] = 0.0;
-//		T.m_left.m_a[1][1] = 1.0;
-//		T.m_left.m_a[1][2] = 0.0;
-//		T.m_left.m_b[1] = T.m_b[1];
-//		T.m_left.m_a[2][0] = 0.0;
-//		T.m_left.m_a[2][1] = 0.0;
-//		T.m_left.m_a[2][2] = 1.0;
-//		T.m_left.m_b[2] = T.m_b[0];
-//		T.m_right.m_a[1][0] = 0.0;
-//		T.m_right.m_a[1][1] = 1.0;
-//		T.m_right.m_a[1][2] = 0.0;
-//		T.m_right.m_b[1] = T.m_b[0];
-//		T.m_right.m_a[2][0] = 0.0;
-//		T.m_right.m_a[2][1] = 0.0;
-//		T.m_right.m_a[2][2] = 1.0;
-//		T.m_right.m_b[2] = T.m_b[2];
-		
-		T.m_left.m_x[1] = T.m_x[0];
-		T.m_left.m_x[2] = T.m_x[1];
-		
-		T.m_left.m_x[0] = (T.m_left.m_b[0] - T.m_left.m_a[0][1] * T.m_left.m_x[1] - T.m_left.m_a[0][2] * T.m_left.m_x[2])
+
+		T.m_left.m_x[1] = T.m_x[1];
+		T.m_left.m_x[2] = T.m_x[0];
+
+		T.m_left.m_x[0] = (T.m_left.m_b[0] - T.m_left.m_a[0][1]
+				* T.m_left.m_x[1] - T.m_left.m_a[0][2] * T.m_left.m_x[2])
 				/ T.m_left.m_a[0][0];
-		
-		T.m_right.m_x[1] = T.m_x[1];
+
+		T.m_right.m_x[1] = T.m_x[0];
 		T.m_right.m_x[2] = T.m_x[2];
 		
-		T.m_right.m_x[0] = (T.m_right.m_b[0] - T.m_right.m_a[0][1] * T.m_right.m_x[1] - T.m_right.m_a[0][2] * T.m_right.m_x[2])
-				/ T.m_right.m_a[0][0];
+
 		
 		return T;
 	}

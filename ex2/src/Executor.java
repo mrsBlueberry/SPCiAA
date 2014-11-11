28568e17f3b191c7e671443c049a307752c2a803
Anna <anna.jagodzinska91@gmail.com>
2014-11-11 17:59:34 +0100

    Refactoring of Executor and changed printing images

--- ex2/src/Executor.java
+++ ex2/src/Executor.java
@@ -5,120 +5,174 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @(#)Executor.java
- *
- *
+ * 
+ * 
  * @author
  * @version 1.00 2011/6/13
  */
 
-
 class Executor extends Thread {
 	private int power = 3;
 	List<List<Vertex>> treesByLevel = new ArrayList<>();
-	
+	private CyclicBarrier barrier;
+
+	private void makeBarrier(int nodes) {
+		if (barrier != null) {
+			throw new RuntimeException("Nested barrier");
+		}
+		barrier = new CyclicBarrier(nodes + 1);
+	}
+
+	private void sync() {
+		try {
+			barrier.await();
+			barrier = null;
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private List<Vertex> level(int lvl) {
+		return treesByLevel.get(lvl);
+	}
+
+	private int levelSize(int lvl) {
+		return level(lvl).size();
+	}
+
+	private int height() {
+		return treesByLevel.size();
+	}
+
+	private List<Vertex> leaves() {
+		return level(height() - 1);
+	}
+
+	private int leafCount() {
+		return leaves().size();
+	}
+
+	private Vertex firstLeaf() {
+		return leaves().get(0);
+	}
+
+	private Vertex lastLeaf() {
+		return leaves().get(leafCount() - 1);
+	}
+
+	private Vertex root() {
+		return level(0).get(0);
+	}
 
 	public synchronized void run() {
 
 		Vertex S = new Vertex(null, null, null, "S");
 
-		CyclicBarrier barrier = new CyclicBarrier(2);
+		makeBarrier(1);
 		P1 p1 = new P1(S, barrier);
 		p1.start();
-		try {
-			barrier.await();
+		sync();
 
-			make2kTree(p1);
-			traverse(p1.m_vertex, 0);
-			int idx = treesByLevel.size() - 1;
-			barrier = new CyclicBarrier(treesByLevel.get(idx).size() + 1);
-			A1 a1 = new A1(treesByLevel.get(idx).get(0), barrier);
+		make2kTree(p1);
+		traverse(p1.m_vertex, 0);
+		for (int t = 0; t < 1000; ++t) {
+
+			makeBarrier(leafCount());
+			A1 a1 = new A1(firstLeaf(), barrier);
 			a1.start();
-			for (int i = 1; i < treesByLevel.get(idx).size() - 1; ++i) {
-				A a = new A(treesByLevel.get(idx).get(i), barrier);
+			for (int i = 1; i < leafCount() - 1; ++i) {
+				A a = new A(leaves().get(i), barrier);
 				a.start();
 			}
-			AN an = new AN(treesByLevel.get(idx).get(
-					treesByLevel.get(idx).size() - 1), barrier);
+			AN an = new AN(lastLeaf(), barrier);
 			an.start();
+			sync();
 
-			barrier.await();
-			for (int i = idx - 1; i > 0; --i) {
-				barrier = new CyclicBarrier(treesByLevel.get(i).size() + 1);
-				for (Vertex v : treesByLevel.get(i)) {
+			for (int i = height() - 2; i > 0; --i) {
+				makeBarrier(levelSize(i));
+				for (Vertex v : level(i)) {
 					A2 a2 = new A2(v, barrier);
 					a2.start();
 				}
+				sync();
 
-				barrier.await();
-				barrier = new CyclicBarrier(treesByLevel.get(i).size() + 1);
-				for (Vertex v : treesByLevel.get(i)) {
+				makeBarrier(levelSize(i));
+				for (Vertex v : level(i)) {
 					E2 e2 = new E2(v, barrier);
 					e2.start();
 				}
-				barrier.await();
+				sync();
 			}
-			barrier = new CyclicBarrier(2);
-			Aroot aRoot = new Aroot(treesByLevel.get(0).get(0), barrier);
-			aRoot.start();
-			barrier.await();
-			barrier = new CyclicBarrier(2);
-			Eroot eRoot = new Eroot(treesByLevel.get(0).get(0), barrier);
-			eRoot.start();
-			barrier.await();
 
-			for (int i = 0; i < idx; ++i) {
-				barrier = new CyclicBarrier(treesByLevel.get(i).size() + 1);
-				for (Vertex v : treesByLevel.get(i)) {
+			makeBarrier(1);
+			Aroot aRoot = new Aroot(root(), barrier);
+			aRoot.start();
+			sync();
+
+			makeBarrier(1);
+			Eroot eRoot = new Eroot(root(), barrier);
+			eRoot.start();
+			sync();
+
+			for (int i = 0; i < height() - 1; ++i) {
+				makeBarrier(levelSize(i));
+				for (Vertex v : level(i)) {
 					BS b = new BS(v, barrier);
 					b.start();
 				}
-				barrier.await();
+				sync();
 			}
 
-			List<Double> result = new ArrayList<>();
-			System.out.println("x0" + "="
-					+ treesByLevel.get(idx).get(0).m_x[1]);
-//			result.add(treesByLevel.get(idx).get(0).m_x[1]);
-			for (int i = 0; i < treesByLevel.get(idx).size(); ++i) {
-//				System.out.println("x0" + "="
-//						+ treesByLevel.get(idx).get(i).m_x[1]);
-//				System.out.println("x" + i + "="
-//						+ treesByLevel.get(idx).get(i).m_x[2]);
-				result.add(treesByLevel.get(idx).get(i).m_x[1]);
-
+			// Output
+			if (t % 100 == 0) {
+				List<Double> result = new ArrayList<>();
+				for (int i = 0; i < leafCount(); ++i) {
+					result.add(leaves().get(i).m_x[1]);
+				}
+				result.add(lastLeaf().m_x[2]);
+				ResultPrinter.printResult(result);
+				delay(1000);
 			}
-			result.add(treesByLevel.get(idx).get(treesByLevel.get(idx).size()-1).m_x[2]);
-			ResultPrinter.printResult(result);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			// TODO Auto-generated catch block
+		}
+
+		System.out.println("Done.");
+
+	}
+
+	private void delay(int ms) {
+		try {
+			TimeUnit.MILLISECONDS.sleep(ms);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
+			Thread.currentThread().interrupt();
 		}
 	}
-	
-	private void traverse(Vertex v, int level){
-		if(v == null){
+
+	private void traverse(Vertex v, int level) {
+		if (v == null) {
 			return;
 		}
-		if(treesByLevel.size()== level){
+		if (treesByLevel.size() == level) {
 			treesByLevel.add(new ArrayList<Vertex>());
 		}
 		treesByLevel.get(level).add(v);
-		traverse(v.m_left, level+1);
-		traverse(v.m_right, level+1);
+		traverse(v.m_left, level + 1);
+		traverse(v.m_right, level + 1);
 	}
 
 	private void make2kTree(Production production) {
-		Queue<Production> queue = new LinkedList();
+		Queue<Production> queue = new LinkedList<>();
 		queue.add(production);
 		int k = 2;
 		while (!queue.isEmpty()) {
 			Iterator<Production> i = queue.iterator();
-			List<Production> l = new LinkedList();
+			List<Production> l = new LinkedList<>();
 			int size = queue.size();
-			CyclicBarrier barrier = new CyclicBarrier(size*2+1);
+			CyclicBarrier barrier = new CyclicBarrier(size * 2 + 1);
 			while (i.hasNext()) {
 				l.add(queue.poll());
 			}
@@ -147,4 +201,3 @@ class Executor extends Thread {
 		}
 	}
 }
-
